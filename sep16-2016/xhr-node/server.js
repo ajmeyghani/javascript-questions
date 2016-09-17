39380b81cef15a93b3e5513be9ebc985a56aee4b
Amin Meyghani <meyghania@gmail.com>
2016-09-16 21:10:20 -0700

    add some notes on npm and git.

--- sep16-2016/xhr-node/server.js
+++ sep16-2016/xhr-node/server.js
@@ -1,3 +1,23 @@
+/*
+ npm init: creates the package.json file
+ npm install express --save: install the express module and adds it as a prod dep
+ npm install cors --save: installed the cors plugin
+ npm install mocha --save-dev: installed as a dev dep
+
+
+ deploy:
+ npm install --production: installs the prod deps
+
+ Git: ignore the node_modules folder
+ .gitignore: specify the folders you want to ignore
+
+ // GIT:
+ // code repo: collabroate, versioning
+  git status: what has changed, working directory details
+  git diff: tell me the changes so far
+  git add: adding files that you want to commit
+  git commit: save your work (saving work)
+*/
 var express = require('express');
 var app = express();
 var cors = require('cors');
