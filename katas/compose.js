b8246c4f6cc17679a433cca4f11406823e204be2
Amin Meyghani <meyghania@gmail.com>
2016-09-26 11:25:03 -0700

    add some comments explaining what compose does.

--- katas/compose.js
+++ katas/compose.js
@@ -14,6 +14,15 @@ multiplies the input by 5 after adding 10 to it.
 const compose = (f, g) => x => f(g(x));
 const add = a => b => a + b;
 const mult5 = a => a * 5;
-const mult5AfterAdd10 = compose(mult5, add(10));
+
+const add10 = add(10);
+const mult5AfterAdd10 = compose(mult5, add10);
+
+// when compose is called, a new function is created
+// that take a single parameter, when this function
+// is called, the input is first passed to add10 -> x + 10
+// and the result of that is called by mult5:
+// mult5(x + 10). If x = 2, then mult5(2 + 5) -> mult5(7) -> 35.
+
 
 module.exports = mult5AfterAdd10;
